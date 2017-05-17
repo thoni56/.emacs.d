363d8320c833e313fcd5c072bd1cf3443f69a8fe
Thomas Nilefalk <thomas@junovagen.se>
2017-05-17 08:42:04 +0200

    Initial setup with el-get and basic indentation

---
+++ init.el
@@ -0,0 +1,16 @@
+;;; El-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+;;; Formatting and indentation
+(setq-default tab-width 4)
+(setq c-basic-offset 4)
+(setq c-tab-always-indent t)
+(setq-default indent-tabs-mode nil)
